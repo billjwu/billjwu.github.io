98ba146a41334fc9b958121455011ff5b33ae848
bewjai <billwu1012@gmail.com>
2021-04-08 22:04:05 -0700

    finished website design and functionalities

---
+++ scripts/scripts.js
@@ -0,0 +1,15 @@
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/bjwu/");
+}
+function openGithub() {
+    window.open("https://github.com/billjwu");
+}
+function openResume() {
+    window.open("../Bill_Wu_Resume.pdf");
+}
+function openPomo() {
+    window.open("https://github.com/elliscchang/cse110-w21-group8");
+}
+function openShop() {
+    window.open("https://github.com/billjwu/Lab6_CSE110Shop");
+}
