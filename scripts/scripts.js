4fe9e505575094ed00609dc57dcff12b57d16d9e
bewjai <billwu1012@gmail.com>
2021-05-02 08:06:24 -0700

    added links to websites

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,6 +10,12 @@ function openResume() {
 function openPomo() {
     window.open("https://github.com/elliscchang/cse110-w21-group8");
 }
+function openPomoWebsite() {
+    window.open("https://elliscchang.github.io/cse110-w21-group8/");
+}
 function openShop() {
     window.open("https://github.com/billjwu/Lab6_CSE110Shop");
+}
+function openShopWebsite() {
+    window.open("https://billjwu.github.io/Lab6_CSE110Shop/");
 }
